781caa8629bca08af0270668a06f67c1bef4abbf
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-05-05 02:56:30 +0900

    ダイアログで見れるように、あとパラメータとか全部見えるように修正しました

--- app/src/main/java/com/taku/kobayashi/referrersample/MainActivity.kt
+++ app/src/main/java/com/taku/kobayashi/referrersample/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.taku.kobayashi.referrersample
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.play.core.review.ReviewManagerFactory
 
 class MainActivity : Activity() {
 
@@ -11,25 +14,66 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // ディープリンクで起動された場合じゃない時はこのデフォルトの文言が使われる
-        var message = getString(R.string.standard_message)
-
+        val messagesList = ArrayList<String>()
         // ディープリンクから起動されたことを取得する
         val intent = intent
         val action = intent.action
         if (Intent.ACTION_VIEW == action) {
             // 呼び出されたディープリンクのURLを取得する
             val uri = intent.data
+            messagesList.add("url:${uri.toString()}")
             if(uri != null){
-                message = getString(R.string.deeplink_received_params)
+                messagesList.add(getString(R.string.deeplink_received_params))
                 // URLの中に含まれているパラメータ(URLクエリ)の情報を取得して表示する
                 val urlQuery = uri.query
+                messagesList.add("urlQuery:$urlQuery")
                 for (name in uri.queryParameterNames) {
-                    message += name + ":" + uri.getQueryParameter(name) + "\n"
+                    messagesList.add(name + ":" + uri.getQueryParameter(name))
                 }
+                showStoreReview()
+            }
+        }else{
+            // ディープリンクで起動された場合じゃない時はこのデフォルトの文言が使われる
+            messagesList.add(getString(R.string.standard_message))
+        }
+        val textView = findViewById<TextView>(R.id.RecievedParams)
+        textView.text = messagesList.joinToString("\n")
+    }
+
+    private fun showStoreReview(){
+        val reviewMessagesList = ArrayList<String>()
+        reviewMessagesList.add("ReviewRequest")
+        val manager = ReviewManagerFactory.create(this)
+        val request = manager.requestReviewFlow()
+        request.addOnCompleteListener { task ->
+            reviewMessagesList.add("requestReviewFlow")
+            reviewMessagesList.add(arrayOf("isComplete", task.isComplete.toString(), "isSuccessful", task.isSuccessful.toString()).joinToString(":"))
+            if (task.isSuccessful) {
+                val reviewInfo = task.result
+                reviewMessagesList.add("requestReviewFlowResult:${reviewInfo.toString()}")
+                reviewMessagesList.add("describeContents:${reviewInfo.describeContents()}")
+                val flow = manager.launchReviewFlow(this, reviewInfo)
+                flow.addOnCompleteListener {
+                    reviewMessagesList.add("launchReviewFlow:${reviewInfo.toString()}")
+                    reviewMessagesList.add(arrayOf("isComplete", flow.isComplete.toString(), "isSuccessful", flow.isSuccessful.toString()).joinToString(":"))
+                    reviewMessagesList.add(arrayOf("flowResult", flow.result).joinToString(":"))
+                    showAlertDialog("StoreReview Result Success", reviewMessagesList.joinToString("\n"))
+                }
+            } else {
+                val exception = task.exception
+                reviewMessagesList.add("message:" + exception?.message)
+                reviewMessagesList.add("localizedMessage:" + exception?.localizedMessage)
+                reviewMessagesList.add("stackTraceToString:" + exception?.stackTraceToString())
+                showAlertDialog("StoreReview Result Error", reviewMessagesList.joinToString("\n"))
             }
         }
-        val text = findViewById(R.id.RecievedParams) as TextView
-        text.text = message
+    }
+
+    private fun showAlertDialog(title: String, message: String = "", onClick: DialogInterface.OnClickListener? = null){
+        AlertDialog.Builder(this) // FragmentではActivityを取得して生成
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", onClick)
+                .show()
     }
 }
--- app/src/main/java/com/taku/kobayashi/referrersample/ReferrerReciever.kt
+++ app/src/main/java/com/taku/kobayashi/referrersample/ReferrerReciever.kt
@@ -11,21 +11,28 @@ class ReferrerReciever : BroadcastReceiver() {
 
     // Referrerを受け取るとここが呼ばれる
     override fun onReceive(context: Context, intent: Intent) {
+        val messagesList = ArrayList<String>()
+        messagesList.add("onReceiveIntent")
         //どんなものを受け取ったかわかるようにKeyとValueの値の一覧をLogに吐き出す。
         val bundle = intent.extras
         if(bundle != null){
             for (key in bundle.keySet()) {
-                Log.d(TAG, key + ":" + bundle.get(key).toString())
+                val kvm = key + ":" + bundle.get(key).toString()
+                messagesList.add(kvm)
+                Log.d(TAG, kvm)
             }
         }
 
+        messagesList.add("")
+        messagesList.add("referrerMessage")
         // Referrerを取得
         val referrer = intent.getStringExtra("referrer")
-        sendNotification(context, referrer)
+        messagesList.add(referrer.toString())
+        sendNotification(context, messagesList.joinToString("\n"))
     }
 
     // 受け取ったReferrerをNotificationとして通知する処理
-    private fun sendNotification(context: Context, message: String?) {
+    private fun sendNotification(context: Context, message: String) {
         Notify.with(context).content {
             title = context.getString(R.string.referer_title)
             text = message
