5e35870c374b757dc3b1bba7703bff7ac79c8ae8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-05-09 01:34:51 +0900

    DeepLinkをカスタマイズしてリンクを踏めるようにしてみた

--- landingpage/src/pages/index.tsx
+++ landingpage/src/pages/index.tsx
@@ -1,21 +1,109 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import { Input, Button } from 'react-rainbow-components'
+import { Divider, TableContainer, Table, TableRow, TableCell, TableBody } from '@material-ui/core'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
-const IndexPage = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Container>
-    </Page>
-  </IndexLayout>
-)
+interface UrlParams {
+  key: string
+  value: string
+}
+
+const IndexPage = () => {
+  const [deepLinkParams, setDeepLinkParams] = useState<UrlParams[]>([])
+
+  const deepLinkParamsKeyOnChange = (index: number, inputValue: string): void => {
+    const urlParamsPair = deepLinkParams[index]
+    urlParamsPair.key = inputValue
+    deepLinkParams[index] = urlParamsPair
+    setDeepLinkParams([...deepLinkParams])
+  }
+
+  const deepLinkParamsValueOnChange = (index: number, inputValue: string): void => {
+    const urlParamsPair = deepLinkParams[index]
+    urlParamsPair.value = inputValue
+    deepLinkParams[index] = urlParamsPair
+    setDeepLinkParams([...deepLinkParams])
+  }
+
+  const deepLinkDeleteParamsButtonOnClick = (index: number): void => {
+    deepLinkParams.splice(index, 1)
+    setDeepLinkParams([...deepLinkParams])
+  }
+
+  const deepLinkAddParamsButtonOnClick = () => {
+    setDeepLinkParams([...deepLinkParams, { key: '', value: '' }])
+  }
+
+  const deeplinkRootUrl = 'referrersample://thisissample'
+
+  let deepLinkUrl = deeplinkRootUrl
+  if (deepLinkParams.length > 0) {
+    const urlParamsPairStrings: String[] = []
+    for (const params of deepLinkParams) {
+      if (params.key && params.key.length > 0) {
+        urlParamsPairStrings.push([params.key, params.value].join('='))
+      }
+    }
+    if (urlParamsPairStrings.length > 0) {
+      deepLinkUrl = deepLinkUrl + '?' + urlParamsPairStrings.join('&')
+    }
+  }
+
+  return (
+    <IndexLayout>
+      <Page>
+        <Container>
+          <div style={{ fontSize: '24px' }}>
+            <b style={{ color: '#FF0000' }}>Deeplink URL:</b> <a href={deepLinkUrl}>{deepLinkUrl}</a>
+          </div>
+          <Divider />
+          <TableContainer>
+            <Table>
+              <TableBody>
+                {deepLinkParams.map((urlPrams, index) => {
+                  return (
+                    <TableRow>
+                      <TableCell>
+                        <Input
+                          placeholder="Key"
+                          type="text"
+                          className="rainbow-p-around_medium"
+                          onChange={event => deepLinkParamsKeyOnChange(index, event.target.value)}
+                          value={urlPrams.key}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Input
+                          placeholder="Value"
+                          type="text"
+                          className="rainbow-p-around_medium"
+                          onChange={event => deepLinkParamsValueOnChange(index, event.target.value)}
+                          value={urlPrams.value}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          label="-"
+                          variant="destructive"
+                          className="rainbow-m-around_medium"
+                          onClick={() => deepLinkDeleteParamsButtonOnClick(index)}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Button label="+" variant="border-filled" className="rainbow-m-around_medium" onClick={deepLinkAddParamsButtonOnClick} />
+        </Container>
+      </Page>
+    </IndexLayout>
+  )
+}
 
 export default IndexPage
