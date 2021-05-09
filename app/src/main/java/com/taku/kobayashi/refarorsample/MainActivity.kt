7a24c60820d0784b72183406ab427fc3067cc4ec
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-05-10 04:25:36 +0900

    Install Referrerが機能しなくなっちゃっていたので新しく機能する形を実装してみた

--- app/src/main/java/com/taku/kobayashi/refarorsample/MainActivity.kt
+++ app/src/main/java/com/taku/kobayashi/refarorsample/MainActivity.kt
@@ -5,10 +5,15 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.android.installreferrer.api.InstallReferrerClient
+import com.android.installreferrer.api.InstallReferrerStateListener
+import com.android.installreferrer.api.ReferrerDetails
 import com.google.android.play.core.review.ReviewManagerFactory
 
 class MainActivity : Activity() {
+    private var renderTextViewMessage: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +41,14 @@ class MainActivity : Activity() {
             // ディープリンクで起動された場合じゃない時はこのデフォルトの文言が使われる
             messagesList.add(getString(R.string.standard_message))
         }
+        renderTextViewMessage = renderTextView(messagesList.joinToString("\n"))
+        startInstallReferrerService()
+    }
+
+    private fun renderTextView(message: String): String {
         val textView = findViewById<TextView>(R.id.RecievedParams)
-        textView.text = messagesList.joinToString("\n")
+        textView.text = message
+        return textView.text.toString()
     }
 
     private fun showStoreReview(){
@@ -57,14 +68,14 @@ class MainActivity : Activity() {
                     reviewMessagesList.add("launchReviewFlow:${reviewInfo.toString()}")
                     reviewMessagesList.add(arrayOf("isComplete", flow.isComplete.toString(), "isSuccessful", flow.isSuccessful.toString()).joinToString(":"))
                     reviewMessagesList.add(arrayOf("flowResult", flow.result).joinToString(":"))
-                    showAlertDialog("StoreReview Result Success", reviewMessagesList.joinToString("\n"))
+                    showToast(reviewMessagesList.joinToString("\n"))
                 }
             } else {
                 val exception = task.exception
                 reviewMessagesList.add("message:" + exception?.message)
                 reviewMessagesList.add("localizedMessage:" + exception?.localizedMessage)
                 reviewMessagesList.add("stackTraceToString:" + exception?.stackTraceToString())
-                showAlertDialog("StoreReview Result Error", reviewMessagesList.joinToString("\n"))
+                showToast(reviewMessagesList.joinToString("\n"))
             }
         }
     }
@@ -75,5 +86,77 @@ class MainActivity : Activity() {
                 .setMessage(message)
                 .setPositiveButton("OK", onClick)
                 .show()
+    }
+
+    private fun showToast(message: String = ""){
+        Toast.makeText(this.applicationContext, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun startInstallReferrerService(){
+        val referrerClient = InstallReferrerClient.newBuilder(this).build()
+        referrerClient.startConnection(object : InstallReferrerStateListener {
+            override fun onInstallReferrerSetupFinished(responseCode: Int) {
+                when (responseCode) {
+                    InstallReferrerClient.InstallReferrerResponse.OK -> {
+                        // Connection established.
+                        showInstallReferrer(referrerClient)
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
+                        // API not available on the current Play Store app.
+                        showToast("API not available on the current Play Store app.")
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
+                        // Connection couldn't be established.
+                        showToast("service unavailable.")
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.PERMISSION_ERROR -> {
+                        // Connection couldn't be established.
+                        showToast("permission error")
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.SERVICE_DISCONNECTED -> {
+                        // Connection couldn't be established.
+                        showToast("service disconnected")
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.DEVELOPER_ERROR -> {
+                        // Connection couldn't be established.
+                        showToast("developer error")
+                    }
+                }
+                referrerClient.endConnection()
+            }
+
+            override fun onInstallReferrerServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+                showToast("InstallReferrerServiceDisconnected")
+            }
+        })
+    }
+
+    private fun showInstallReferrer(referrerClient: InstallReferrerClient){
+        val response: ReferrerDetails = referrerClient.installReferrer
+        val referrerUrl: String = response.installReferrer
+        val referrerClickTime: Long = response.referrerClickTimestampSeconds
+        val appInstallTime: Long = response.installBeginTimestampSeconds
+        val instantExperienceLaunched: Boolean = response.googlePlayInstantParam
+        val installBeginTimestampServerSeconds: Long = response.installBeginTimestampServerSeconds
+        val referrerClickTimestampServerSeconds: Long = response.referrerClickTimestampServerSeconds
+        val installVersion: String = response.installVersion
+
+        val referrerMessagesList = ArrayList<String>()
+        referrerMessagesList.add(renderTextViewMessage)
+        referrerMessagesList.add("referrerUrl:$referrerUrl")
+        referrerMessagesList.add("referrerClickTime:$referrerClickTime")
+        referrerMessagesList.add("appInstallTime:$appInstallTime")
+        referrerMessagesList.add("instantExperienceLaunched:$instantExperienceLaunched")
+        referrerMessagesList.add("installBeginTimestampServerSeconds:$installBeginTimestampServerSeconds")
+        referrerMessagesList.add("referrerClickTimestampServerSeconds:$referrerClickTimestampServerSeconds")
+        referrerMessagesList.add("installVersion:$installVersion")
+        showToast(referrerMessagesList.joinToString("\n"))
+        if(!isFinishing){
+            runOnUiThread {
+                renderTextView(referrerMessagesList.joinToString("\n"))
+            }
+        }
     }
 }
